963bada1095e2116f5a998ff205c82009ea015e0
christian-bromann <mail@christian-bromann.com>
2021-11-05 18:28:40 +0100

    initial commit

---
+++ site/templates/default.php
@@ -0,0 +1,25 @@
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $site->title() ?></title>
+        <?= css('assets/css/style.css') ?>
+        <link rel="icon" type="image/svg+xml" href="<?= $site->url() ?>/assets/images/favicon.svg">
+        <link rel="icon" type="image/png" href="<?= $site->url() ?>/assets/images/favicon.png">
+    </head>
+    <body>
+        <header>
+            <a class="switch" href="#"></a>
+            <nav class="menu">
+            <a class="switch" href="#"></a>
+            </nav>
+        </header>
+        <div id="container">
+            <h1><?= $page->title() ?></h1>
+            <?= $page->text() ?>
+        </div>
+        <footer>
+
+        </footer>
+    </body>
+</html>
