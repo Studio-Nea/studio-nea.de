69c612221a8687d9045ff145de1bdb9b7baf00ef
toschkawitz <anton-meier@posteo.de>
2021-11-11 13:50:28 +0100

    Templates changes

--- site/templates/studio.php
+++ site/templates/studio.php
@@ -3,13 +3,13 @@
                     <a class="menu-link shop" href="#">Shop</a>
                 </div>
                 <div class="menu-item right rotate">
-                    <a class="menu-link contact" href="#">Contact</a>
+                    <a class="menu-link contact" href="#">Start</a>
                 </div>
                 <div class="menu-item bottom horizontal">
                     <a class="menu-link work" href="#">Work</a>
                 </div>
                 <div class="menu-item left rotate">
-                    <a class="menu-link studio" href="#">Studio</a>
+                    <a class="menu-link studio" href="#">Contact</a>
                 </div>           
             </nav>
         </header>
