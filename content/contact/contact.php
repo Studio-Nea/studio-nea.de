ff739e2a8aca91271ba0d1e3839460ba83509745
toschkawitz <anton-meier@posteo.de>
2021-11-12 10:30:10 +0100

    Contact menu changes

--- content/contact/contact.php
+++ content/contact/contact.php
@@ -3,13 +3,13 @@
                     <a class="menu-link shop" href="<?= $site->url() ?>/shop">Shop</a>
                 </div>
                 <div class="menu-item right rotate">
-                    <a class="menu-link contact" href="<?= $site->url() ?>/studio">Studio</a>
+                    <a class="menu-link studio" href="<?= $site->url() ?>/studio">Studio</a>
                 </div>
                 <div class="menu-item bottom horizontal">
                     <a class="menu-link work" href="<?= $site->url() ?>/work">Work</a>
                 </div>
                 <div class="menu-item left rotate">
-                    <a class="menu-link studio" href="<?= $site->url() ?>">Start</a>
+                    <a class="menu-link start" href="<?= $site->url() ?>">Start</a>
                 </div>           
             </nav>
         </header>
