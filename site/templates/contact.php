f65329191d1b19d6b6d7657ab8d766cdd1269ee2
toschkawitz <anton-meier@posteo.de>
2021-11-18 09:12:47 +0100

    Menu changes

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,12 +1,6 @@
 <?php snippet('header') ?>
-                <div class="menu-item top horizontal">
-                    <a class="menu-link shop" href="<?= $site->url() ?>/shop">Shop</a>
-                </div>
                 <div class="menu-item right rotate">
                     <a class="menu-link studio" href="<?= $site->url() ?>/studio">Studio</a>
-                </div>
-                <div class="menu-item bottom horizontal">
-                    <a class="menu-link work" href="<?= $site->url() ?>/work">Work</a>
                 </div>
                 <div class="menu-item left rotate">
                     <a class="menu-link start" href="<?= $site->url() ?>">Start</a>
