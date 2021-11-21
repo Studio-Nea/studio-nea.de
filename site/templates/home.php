d39d4a85adf19a0c34ce3d162952365404ba0e17
toschkawitz <anton-meier@posteo.de>
2021-11-21 18:56:21 +0100

    Create single home.php template

---
+++ site/templates/home.php
@@ -0,0 +1,24 @@
+<?php snippet('header') ?>
+                <div class="menu-item top horizontal">
+                    <a class="menu-link shop" href="<?= $site->url() ?>/shop">Shop</a>
+                </div>
+                <div class="menu-item right rotate">
+                    <a class="menu-link contact" href="<?= $site->url() ?>/contact">Contact</a>
+                </div>
+                <div class="menu-item bottom horizontal">
+                    <a class="menu-link work" href="<?= $site->url() ?>/work">Work</a>
+                </div>
+                <div class="menu-item left rotate">
+                    <a class="menu-link studio" href="<?= $site->url() ?>/studio">Studio</a>
+                </div>           
+            </nav>
+        </header>
+        <div id="container">
+            <section>
+                <div class="layer">
+                    <h1><?= $page->title() ?></h1>
+                    <?= $page->text() ?>
+                </div>
+            </section>
+        </div>
+<?php snippet('footer') ?>
